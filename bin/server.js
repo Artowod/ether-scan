9b785d77e0a437f871f17002fc8afa406372f633
Artowod <myjavaname@gmail.com>
2022-02-16 01:17:53 +0200

    delete comments

--- bin/server.js
+++ bin/server.js
@@ -1,8 +1,3 @@
-#!/usr/bin/env node
-
-/**
- * Module dependencies.
- */
 const mongoose = require("mongoose");
 require("dotenv").config();
 mongoose.Promise = global.Promise;
@@ -12,30 +7,11 @@ const debug = require("debug")("etherscan-back:server");
 const http = require("http");
 const { etheriumCheckInLoop, putFirstBlocksToDB } = require("../services/etherium-services");
 
-/**
- * Get port from environment and store in Express.
- */
-
 const PORT = process.env.PORT || "3001";
-app.set("port", PORT); //????
+app.set("port", PORT);
 
-/**
- * Create HTTP server.
- */
-
-const server = http.createServer(app); //????
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
+const server = http.createServer(app);
 server.listen("3002");
-
-// server.on("error", onError);
-// server.on("listening", () => console.log("Server is online."));
-// server.on("listening", () => {
-//   getRecentBlockNumber();
-// });
 
 try {
   const session = mongoose.connect(process.env.DB_HOST_REMOTE);
@@ -48,92 +24,14 @@ try {
       });
   });
   server.on("listening", async () => {
-    //    await putFirstBlocksToDB(50);
-    // await etheriumCheckInLoop();
-    // const loopId = setInterval(etheriumCheckInLoop, 5000);
-    const loopId = setTimeout(
-      async function () {
-        // await putFirstBlocksToDB(10);
-        // await etheriumCheckInLoop();
-      } /* etheriumCheckInLoop */,
-      10000
-    );
+    const loopId = setTimeout(async function () {
+      await putFirstBlocksToDB(1000);
+      await etheriumCheckInLoop();
+    }, 10000);
   });
 } catch (error) {
   console.log("DB connection Error: ", error);
   process.exit(1);
 }
-//==================================================
-//==================================================
-//==================================================
-
-// server.on("listening", initialDataPushingToDB);
-// server.on("listening", CESCheck);
-
-// server.on("listening", () => {
-//   const recentBlockNum = getRecentBlockNumber();
-//   setTimeout(() => {
-//     getBlockByNumber("0xd87bb8");
-//   }, 2000);
-//   getRecentBlockNumber();
-//   getFirstBlocks(40);
-// });
-
-//????
-//====================================================================
-
-// server.on("listening", () => {
-//   const loopId = setInterval(etheriumCheckInLoop, 5000);
-// });
-
-//====================================================================
-// server.on("listening", () => {
-//   getTransactionsByBlockNumber("0xD860F2");
-// });
-
-// server.on("listening", () => {
-//   getTransactionByHash("4ce799d88a1c1d0f4bfcda5645be8275d14e8daf5da969a28fa8161d604e3cc6");
-// });
-
-//==================================================
-//==================================================
-//==================================================
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-// function onError(error) {
-//   if (error.syscall !== "listen") {
-//     throw error;
-//   }
-
-//   var bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
-
-//   // handle specific listen errors with friendly messages
-//   switch (error.code) {
-//     case "EACCES":
-//       console.error(bind + " requires elevated privileges");
-//       process.exit(1);
-//       break;
-//     case "EADDRINUSE":
-//       console.error(bind + " is already in use");
-//       process.exit(1);
-//       break;
-//     default:
-//       throw error;
-//   }
-// }
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-// function onListening() {
-//   console.log("HI!");
-//   var addr = server.address();
-//   var bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-//   debug("Listening on " + bind);
-// }
 
 module.exports = mongoose;
